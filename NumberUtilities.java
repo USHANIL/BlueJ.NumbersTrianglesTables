c913c96589ed00f85db65811405ba843a7f3ddd4
Usha Kunapareddy <usha@zipcoders-MacBook-Pro-7.local>
2019-10-19 05:40:57 -0400

    modified java files

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -3,30 +3,85 @@
 
 public class NumberUtilities {
 
-    public static String getRange(int stop) {
-        return null;
+    public static String getRange(int stop) 
+    {
+        String conString = "";
+        for (int i = 0 ; i< stop ; i++)
+        {
+        conString = conString + i;
+        //conString = String.format(conString,i);
+        
+        }
+        return conString;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String conString = "";
+        for (int i = start ; i< stop ; i++)
+        {
+        conString = conString + i;
+        //conString = String.format(conString,i);
+        
+        }
+        return conString;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String conString = "";
+        for (int i = start ; i< stop ;)
+        {
+        conString = conString + i;
+        //conString = String.format(conString,i);
+        i = i + step;
+        }
+        return conString;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String conString = "";
+        if (start%2 != 0 )
+        {
+            start = start + 1;
+        }
+        for (int i = start ; i< stop ;)
+        {
+        conString = conString + i;
+        //conString = String.format(conString,i);
+        i = i + 2;
+        }
+        return conString;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String conString = "";
+        if (start%2 == 0 )
+        {
+            start = start + 1;
+        }
+        for (int i = start ; i< stop ;)
+        {
+        conString = conString + i;
+       
+        i = i + 2;
+        }
+        return conString;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String conString = "";
+        
+        for (int i = start ; i<= stop ; i++)
+        {
+            int expValue = (int) Math.pow(i, exponent);
+            conString = conString + expValue;
+            //conString = String.format("%s, %.2f",conString , expValue );
+        //conString = String.format(conString,i);
+        
+        
+        }
+        return conString;
     }
 }
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,72 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String conString = "";
+        for (int i = 0 ; i< numberOfStars ; i++)
+        {
+        conString = conString + "*";
+        //conString = String.format(conString,i);
+        
+        }
+        return conString;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String conString = "";
+        for (int i = 0 ; i< numberOfRows ; i++)
+        {
+            String noOfStars = "";
+            
+                for ( int j=0 ; j <= i ; j++)
+                {
+                    noOfStars = noOfStars + "*" ;
+                }
+            
+            conString = conString + noOfStars + "\n";
+            
+        //conString = String.format(conString,i);
+        
+        }
+        //System.out.println(conString);
+        return conString;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        String conString = "";
+        for (int i = 0 ; i< 4 ; i++)
+        {
+            String noOfStars = "";
+            
+                for ( int j=0 ; j <= i ; j++)
+                {
+                    noOfStars = noOfStars + "*" ;
+                }
+            
+            conString = conString + noOfStars + "\n";
+            
+        //conString = String.format(conString,i);
+        
+        }
+        return conString;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String conString = "";
+        for (int i = 0 ; i< 9 ; i++)
+        {
+            String noOfStars = "";
+            
+                for ( int j=0 ; j <= i ; j++)
+                {
+                    noOfStars = noOfStars + "*" ;
+                }
+            
+            conString = conString + noOfStars + "\n";
+            
+        //conString = String.format(conString,i);
+        
+        }
+        return conString;
     }
 }
